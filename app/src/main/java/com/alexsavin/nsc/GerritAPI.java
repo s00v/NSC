63b82e52bc8ab3d4b2c375df2b5dd7023b8dbcfe
s00v <73668429+s00v@users.noreply.github.com>
2022-08-04 21:59:05 +0400

    add new retrofit classes

---
+++ app/src/main/java/com/alexsavin/nsc/GerritAPI.java
@@ -0,0 +1,12 @@
+package com.alexsavin.nsc;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface GerritAPI {
+    @GET("changes/")
+    Call<List<Change>> loadChanges(@Query("q") String status);
+}
